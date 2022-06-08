65cd61daad074e0373a763a846007fd92874566b
Tony Huang <yiye_huang@apple.com>
2022-06-08 14:32:36 -0700

    init

---
+++ publicRepoTestAppGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  publicRepoTestAppGH
+//
+//  Created by YIYE HUANG on 2022-06-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ publicRepoTestAppGH/publicRepoTestAppGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  publicRepoTestAppGHApp.swift
+//  publicRepoTestAppGH
+//
+//  Created by YIYE HUANG on 2022-06-08.
+//
+
+import SwiftUI
+
+@main
+struct publicRepoTestAppGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
